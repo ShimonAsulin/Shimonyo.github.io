822834795b646344a18099f8881a8d2780585590
Shimon asulin <62749609+Shimonyo@users.noreply.github.com>
2022-02-02 22:31:58 +0200

    Add files via upload

--- quick-WhatApp/scripts.js
+++ quick-WhatApp/scripts.js
@@ -1,5 +1,31 @@
-function othername() {
-    var input = document.getElementById("userInput").value;
-    var WhatsApp_link = "https://wa.me/+972" + input;
-    window.location = WhatsApp_link;
+const header = document.getElementById('header');
+let dropdown = document.getElementById('dropdown');
+let input = document.getElementById('input');
+const button = document.getElementById('send');
+let user =  localStorage.getItem("user");
+const edit = document.getElementById('edit-name');
+if (!user) {    
+     user = prompt("Please enter your name😎")
 }
+
+localStorage.setItem("user", user )
+header.textContent += user;
+// collect
+let quickWhatsApp = null
+button.addEventListener("click", () => {
+    quickWhatsApp = `https://wa.me/${dropdown.value}${input.value}`
+    window.open(quickWhatsApp, '_blank');
+    // alert(quickWhatsApp);
+})
+// keyboard key
+input.addEventListener("keyup", (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    document.getElementById('send').click();
+  }
+});
+// change name
+edit.addEventListener("click", () => {
+    localStorage.clear()
+    location.reload();
+})

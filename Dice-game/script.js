5995c3033fc7cf53b1f8986129bbb69a5d87825b
Shimon asulin <asulin.shimon699@gmail.com>
2022-02-21 17:45:04 +0200

    /

---
+++ Dice-game/script.js
@@ -0,0 +1,101 @@
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+let player1Counter = 0
+let player2Counter = 0
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const double = document.getElementById("DON")
+
+
+
+// functions
+
+function playerCondition() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+        player1Counter++
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+        player2Counter++
+    }
+    console.log(player1Counter)
+    console.log(player2Counter)
+    if (player1Score >= 20 && player2Score >= 20) {
+        message.textContent = "You both got over 20 points, but a winner has only one! want to try again?🤼‍♂️"
+        showResetButton()
+    }
+        else if (player1Score >= 20 && player1Counter === player2Counter) {
+            message.textContent = "Player 1 Won 🥳"
+            showResetButton()
+    }   else if (player2Score >= 20) {
+            message.textContent = "Player 2 Won 🎉"
+            showResetButton()
+    }
+    player1Turn = !player1Turn
+}
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+    DON.style.display = "none"
+}
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Counter = 0
+    player2Counter = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    DON.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
+
+function doubleOrNothing() {
+
+}
+
+// double or nothing
+
+double.addEventListener("dblclick", function() {
+    
+})
+
+
+
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", function() {
+    playerCondition()
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+    
+})
+
--- Password Generator/index.js
+++ Password Generator/index.js
@@ -1,12 +1,5 @@
 
 
-// const btnGenerate = document.getElementById('btnGenerate');
-// let input1 = document.getElementById('input1')
-// let input2 = document.getElementById('input2')
-// let input3 = document.getElementById('input3')
-// let input4 = document.getElementById('input4')
-
-
 function copyToClipboard(event) {
     const input = event.target;
     if (!!input.value) {
@@ -52,14 +45,4 @@ textInputElements.forEach(input => input.addEventListener('click', (copyToClipbo
 btnGenerate.addEventListener('click', () => {
     textInputElements.forEach((input => generate(input)));
 })    
-
-
-
-
-
-
-
-
-
-
 

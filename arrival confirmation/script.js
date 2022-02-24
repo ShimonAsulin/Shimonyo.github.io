cc57fcf47798355847e68581b23f0bc5e593999f
Shimon asulin <asulin.shimon699@gmail.com>
2022-02-24 15:35:45 +0200

    .

---
+++ arrival confirmation/script.js
@@ -0,0 +1,49 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFzWDmhcthHIFAlejDQRqC7yhNlqpfVvY",
+    authDomain: "contactform-85272.firebaseapp.com",
+    databaseURL: "https://contactform-85272-default-rtdb.firebaseio.com",
+    projectId: "contactform-85272",
+    storageBucket: "contactform-85272.appspot.com",
+    messagingSenderId: "632260630902",
+    appId: "1:632260630902:web:9199440dc88ef2d0c08257",
+    measurementId: "G-8C3QTYZKTV"
+};
+firebase.initializeApp(firebaseConfig);
+
+const popUp = document.getElementById('popup')
+const container = document.getElementById('container')
+let massagesRef = firebase.database().ref('count')
+
+const contactForm = document.getElementById('contactForm')
+contactForm.addEventListener('submit', submitForm)
+
+//referens massges collaction
+
+// submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    //get values
+    let name = getInputVal('name')
+    let count = getInputVal('count')
+    
+    saveMassage(name, count)
+    popUp.style.display = 'flex'
+    container.style.display = 'none'
+    
+}
+
+// function to get get form value
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+// save masseg
+function saveMassage(name, count) {
+    let newMassageRef = massagesRef.push();
+    newMassageRef.set({
+        name: name,
+        count: count
+    })
+}
+

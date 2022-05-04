1f8eed7a9e2ea78003c2f8cc53756bc56ba5fc77
Shimon asulin <asulin.shimon699@gmail.com>
2022-05-04 11:43:58 +0300

    /

--- Movie Watchlist/script.js
+++ Movie Watchlist/script.js
@@ -12,14 +12,13 @@ const movieListEl = document.getElementById('movie-list')
 
 
 
-async function callApi() {
+ function callApi() {
         let search = inputEl.value
-        console.log("🚀 ~ file: script.js ~ line 6 ~ callApi ~ input", search)
 
     fetch(`https://www.omdbapi.com/?s=${search}&apikey=fcb380f9`)
         .then (res => res.json())
         .then (data =>  {
-            console.log(data);
+            // console.log(data);
             data.Search.map(movie => {
                 fetch(`https://www.omdbapi.com/?t=${movie.Title}&apikey=fcb380f9`)
                 .then(res=>res.json())
@@ -39,17 +38,24 @@ async function callApi() {
 function render(movie) {
         document.getElementById("movie-exploring").style.display = "none"
         movieListEl.innerHTML +=  `
-                <img src="${movie.Poster}">
-                <div>
-                    <div>
-                        <h3>${movie.Title}</hh3>
-                        <p>${movie.imdbRating}</p>
+            <div class="movie-container">
+                <div class="details-1">
+                    <img class="movie-img" src="${movie.Poster}">
+                    <p class="movie-runtime">${movie.Runtime}</p>
+                    <p class="movie-genre">${movie.Genre}</p>
+                    <button id="btn" onclick={addMovie()}>add</button>
+                </div>
+                
+                <div class="details-2">
+                    <div class="title-rating">    
+                            <h3 class="movie-title">${movie.Title}</h3>
+                            <p class="movie-rating">⭐${movie.imdbRating}</p>
                     </div>
-                    <p>${movie.Runtime}</p>
-                    <p>${movie.Genre}</p>
-                    <button id="btn" onclick={addMovie()}>➕</button>
-                    <p>${movie.imdbRating}</p> 
-               </div>
+                        <p class="movie-plot">${movie.Plot}</p>
+                </div>
+                
+                
+            </div>
            `
         
     }
